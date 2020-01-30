772f1b80844a61a08d33464a246eb07652f67d0c
renatopcinfo <renatopcinfo@gmail.com>
2020-01-30 12:47:15 -0300

    frontend

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+  }
+
+  > button {
+    width: 50%;
+
+    margin: 5px 0 0;
+    height: 44px;
+    background: #f64c75;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.08, '#F64C75')};
+    }
+  }
+`;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Search() {
+  return <h1>Faça sua busca</h1>;
+}
