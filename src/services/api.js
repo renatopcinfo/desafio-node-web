13df01fb804b382be936ddb582068c591c6a01c3
renatopcinfo <renatopcinfo@gmail.com>
2020-01-31 17:15:41 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: 'http://165.22.38.31:4000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
