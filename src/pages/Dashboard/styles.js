9af788449734ec7f9a4bbf8870534808aa135408
renatopcinfo <renatopcinfo@gmail.com>
2020-02-01 21:18:45 -0300

    styles

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -2,25 +2,25 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Container = styled.div`
-  max-width: 600px;
-  margin: 50px auto;
+  max-width: 500px;
+  margin: 0px auto;
   }
 
   > button {
     width: 50%;
 
-    margin: 5px 0 0;
+    margin: 50px 80px 0;
     height: 44px;
-    background: #f64c75;
+    background: #ADD8E6;
     font-weight: bold;
-    color: #fff;
+    color: #333;
     border: 0;
     border-radius: 4px;
     font-size: 16px;
     transition: background 0.2s;
 
     &:hover {
-      background: ${darken(0.08, '#F64C75')};
+      background: ${darken(0.08, '#ADD8E6')};
     }
   }
 `;
--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://165.22.38.31:4000',
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export default api;
+
+// baseURL: 'http://165.22.38.31:4000',
+// baseURL: 'http://localhost:4000'
